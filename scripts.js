f38db62a9dfd4b3cda55ef3ad3c6830d5d135ec3
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-22 11:39:08 +0200

    fin affiche fibanacci sur la page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 let button = document.querySelector("button");
-let nombrefi = document.querySelector("p")
-let nombre = 0;
-let nombre2 = 1;
+let section = document.querySelector("section");
 button.addEventListener("click", () => {
+
+    let nombrefi = document.createElement("p")
+    let nombre = 0;
+    nombrefi.textContent = nombre;
+    section.appendChild(nombrefi);
+    let nombre2 = 1;
+    let nombrefib = document.createElement("p")
+    nombrefib.textContent = nombre2
+    section.appendChild(nombrefib);
 
     for (let compteur = 0; compteur < 20; compteur++) {
 
         let somme = nombre + nombre2;
         nombre = nombre2;
         nombre2 = somme;
+        let nombrefi = document.createElement("p")
+        nombrefi.textContent += somme;
         console.log(somme);
-        nombrefi.textContent = somme + compteur;
+        section.appendChild(nombrefi);
     }
-})
+
+    return(button);
+});
